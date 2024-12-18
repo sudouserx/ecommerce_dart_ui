7493738248ec9511ee2991a428633bd984961a87
sudouserx <sudouserx@gmail.com>
2024-12-18 21:12:29 +0530

    intro page

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  'lib/images/fatjordan.png',
+                  height: 240,
+                ),
+              ),
+
+              const SizedBox(height: 48),
+
+              //title
+              Text('Dunk the Doubt',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+
+              const SizedBox(height: 48),
+
+              //sub title
+
+              Text(
+                'Brand new sneakers and custom kicks made with premium quality',
+                style: TextStyle(
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 48),
+
+              //start new button
+              GestureDetector(
+                onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage(),),),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey[900],
+                      borderRadius: BorderRadius.circular(12)),
+                  padding: EdgeInsets.all(25.0),
+                  child: Center(
+                      child: const Text(
+                    'Shop Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  )),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
